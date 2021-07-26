84452fbeaa4a3f73efaf2a954f212bceb60ffe89
ecto0310 <ecto0310@gmail.com>
2021-07-26 22:15:18 +0900

    Add users db

---
+++ backend/db/migrations/20210726215839-create_users.sql
@@ -0,0 +1,15 @@
+-- +migrate Up
+CREATE TABLE users
+(
+    id              BIGINT       NOT NULL AUTO_INCREMENT,
+    name            VARCHAR(64)  NOT NULL,
+    hashed_password VARCHAR(255) NOT NULL,
+    role            VARCHAR(8)   NOT NULL DEFAULT 'member',
+    created_at      DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at      DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (id),
+    UNIQUE (name)
+);
+
+-- +migrate Down
+DROP TABLE users;

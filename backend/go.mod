d8f465a0cba59aaf45506725e650e70f358596fb
ecto0310 <ecto0310@gmail.com>
2021-08-21 01:34:21 +0900

    Add connection to DB

--- backend/go.mod
+++ backend/go.mod
@@ -2,4 +2,7 @@ module github.com/ecto0310/online_judge/backend
 
 go 1.16
 
-require github.com/labstack/echo/v4 v4.3.0
+require (
+	github.com/go-sql-driver/mysql v1.6.0
+	github.com/labstack/echo/v4 v4.3.0
+)
--- backend/main.go
+++ backend/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"github.com/ecto0310/online_judge/backend/pkg/db"
 	"github.com/ecto0310/online_judge/backend/pkg/router"
 )
 
-func main(){
+func main() {
+	db.Init()
+
 	r := router.InitRouter()
 	r.Logger.Fatal(r.Start(":1323"))
 }

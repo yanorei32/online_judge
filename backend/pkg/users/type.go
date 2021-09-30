7aefff77c9e1d6b9f9f4de5f53f726b561dad5f5
ecto0310 <ecto0310@gmail.com>
2021-09-30 23:24:32 +0900

    Add user login

--- backend/pkg/users/type.go
+++ backend/pkg/users/type.go
@@ -1,5 +1,11 @@
 package users
 
+type User struct {
+	Id   int64  `json:"id"`
+	Name string `json:"name"`
+	Role string `json:"role"`
+}
+
 type RegisterData struct {
 	Email    string `json:"email"`
 	Name     string `json:"name"`
@@ -15,4 +21,20 @@ type RegisterResponse struct {
 	Success bool                 `json:"success"`
 	Error   string               `json:"error"`
 	User    RegisterResponseUser `json:"user"`
+}
+
+type LoginData struct {
+	Name     string `json:"name"`
+	Password string `json:"password"`
+}
+
+type LoginResponseUser struct {
+	Name string `json:"name"`
+	Role string `json:"role"`
+}
+
+type LoginResponse struct {
+	Success bool              `json:"success"`
+	Error   string            `json:"error"`
+	User    LoginResponseUser `json:"user"`
 }
--- backend/pkg/users/users.go
+++ backend/pkg/users/users.go
@@ -1,9 +1,11 @@
 package users
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/ecto0310/online_judge/backend/pkg/db"
+	session "github.com/ipfans/echo-session"
 	"github.com/labstack/echo/v4"
 	"golang.org/x/crypto/bcrypt"
 )
@@ -42,7 +44,36 @@ func Register(c echo.Context) error {
 }
 
 func Login(c echo.Context) error {
-	return c.String(http.StatusOK, "Hello, World!")
+	// Get request
+	data := LoginData{}
+	err := c.Bind(&data)
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, RegisterResponse{Success: false, Error: "Can't understand your request."})
+	}
+
+	// Check user
+	hashedPassword := ""
+	validUser := true
+	user := User{}
+	err = db.Db.QueryRow(fmt.Sprintf("SELECT id, name, hashed_password, role FROM users WHERE name='%s'", data.Name)).Scan(&user.Id, &user.Name, &hashedPassword, &user.Role)
+	if err != nil {
+		validUser = false
+		// dummy password
+		hashedPassword = "$2a$10$YvuCJ0AY.IHkE1xjoGZ.VOj8Bkol..cWYE7bgW6ycqaXEHQiP7iZO"
+	}
+	err = bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(data.Password))
+	if !validUser || err != nil {
+		return c.JSON(http.StatusUnauthorized, LoginResponse{Success: false, Error: "Incorrect name or password"})
+	}
+
+	// Generate Response
+	session := session.Default(c)
+	session.Set("id", user.Id)
+	session.Set("name", user.Name)
+	session.Set("role", user.Role)
+	session.Set("login", true)
+	session.Save()
+	return c.JSON(http.StatusOK, LoginResponse{Success: true, Error: "", User: LoginResponseUser{Name: user.Name, Role: user.Role}})
 }
 
 func Logout(c echo.Context) error {

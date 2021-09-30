de69551e7012f1d320eb1f18df195c5e1e6cfb48
ecto0310 <ecto0310@gmail.com>
2021-09-30 20:17:25 +0900

    Add redis session

--- backend/pkg/router/router.go
+++ backend/pkg/router/router.go
@@ -1,7 +1,11 @@
 package router
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/ecto0310/online_judge/backend/pkg/users"
+	session "github.com/ipfans/echo-session"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
@@ -10,6 +14,13 @@ func InitRouter() *echo.Echo {
 	r := echo.New()
 
 	r.Pre(middleware.RemoveTrailingSlash())
+
+	store, err := session.NewRedisStore(32, "tcp", fmt.Sprintf("%s:%s", os.Getenv("REDIS_HOST"), os.Getenv("REDIS_PORT")), os.Getenv("REDIS_PASSWORD"), make([]byte, 32))
+	if err != nil {
+		panic(err)
+	}
+	store.Options(session.Options{Path: "/", MaxAge: 86400 * 7})
+	r.Use(session.Sessions("SESSION", store))
 
 	r.Use(middleware.CORS())
 	r.Use(middleware.Logger())

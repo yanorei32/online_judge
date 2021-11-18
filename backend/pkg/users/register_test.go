2e72b368cf17baa13b28ca226364a529562d3d24
ecto0310 <ecto0310@gmail.com>
2021-11-19 01:24:14 +0900

    Add user register test

---
+++ backend/pkg/users/register_test.go
@@ -0,0 +1,114 @@
+package users_test
+
+import (
+	"bytes"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"regexp"
+	"testing"
+
+	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/ecto0310/online_judge/backend/pkg/test_helper"
+	"github.com/ecto0310/online_judge/backend/pkg/users"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestRegisterSuccess(t *testing.T) {
+	e, db, mock, err := test_helper.CreateTestServer()
+	if err != nil {
+		t.Fatalf("Failed to create mock server '%#v'", err)
+	}
+
+	mock.ExpectExec(regexp.QuoteMeta(`INSERT INTO users (email, name, hashed_password) VALUES (?, ?, ?)`)).
+		WithArgs("admin@example.com", "admin", sqlmock.AnyArg()).
+		WillReturnResult(sqlmock.NewResult(1, 1))
+	mock.ExpectQuery(regexp.QuoteMeta(`SELECT id, name, role FROM users WHERE name=?`)).
+		WithArgs("admin").
+		WillReturnRows(sqlmock.NewRows([]string{"id", "name", "role"}).AddRow(1, "admin", "member"))
+
+	req := httptest.NewRequest(http.MethodPost, "/register", bytes.NewBuffer([]byte("{\"email\": \"admin@example.com\",\"name\": \"admin\",\"password\": \"password\"}")))
+	req.Header.Set("Content-Type", "application/json")
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+
+	users := &users.Users{DB: db}
+	if assert.NoError(t, users.Register(c)) {
+		assert.Equal(t, http.StatusOK, rec.Code)
+		assert.Equal(t, "{\"success\":true,\"error\":\"\",\"user\":{\"id\":1,\"name\":\"admin\",\"role\":\"member\"}}\n", rec.Body.String())
+	}
+	err = mock.ExpectationsWereMet()
+	if err != nil {
+		t.Errorf("Incorrect DB call '%#v'", err)
+	}
+}
+
+func TestRegisterNameValidate(t *testing.T) {
+	e, db, mock, err := test_helper.CreateTestServer()
+	if err != nil {
+		t.Fatalf("Failed to create mock server '%#v'", err)
+	}
+
+	req := httptest.NewRequest(http.MethodPost, "/register", bytes.NewBuffer([]byte("{\"email\": \"admin@example.com\",\"name\": \"\",\"password\": \"password\"}")))
+	req.Header.Set("Content-Type", "application/json")
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+
+	users := &users.Users{DB: db}
+	if assert.NoError(t, users.Register(c)) {
+		assert.Equal(t, http.StatusBadRequest, rec.Code)
+		assert.Equal(t, "{\"success\":false,\"error\":\"insufficient name length\",\"user\":{\"id\":0,\"name\":\"\",\"role\":\"\"}}\n", rec.Body.String())
+	}
+	err = mock.ExpectationsWereMet()
+	if err != nil {
+		t.Errorf("Incorrect DB call '%#v'", err)
+	}
+}
+
+func TestRegisterPasswordValidate(t *testing.T) {
+	e, db, mock, err := test_helper.CreateTestServer()
+	if err != nil {
+		t.Fatalf("Failed to create mock server '%#v'", err)
+	}
+
+	req := httptest.NewRequest(http.MethodPost, "/register", bytes.NewBuffer([]byte("{\"email\": \"admin@example.com\",\"name\": \"admin\",\"password\": \"\"}")))
+	req.Header.Set("Content-Type", "application/json")
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+
+	users := &users.Users{DB: db}
+	if assert.NoError(t, users.Register(c)) {
+		assert.Equal(t, http.StatusBadRequest, rec.Code)
+		assert.Equal(t, "{\"success\":false,\"error\":\"insufficient password length\",\"user\":{\"id\":0,\"name\":\"\",\"role\":\"\"}}\n", rec.Body.String())
+	}
+	err = mock.ExpectationsWereMet()
+	if err != nil {
+		t.Errorf("Incorrect DB call '%#v'", err)
+	}
+}
+
+func TestRegisterDuplicate(t *testing.T) {
+	e, db, mock, err := test_helper.CreateTestServer()
+	if err != nil {
+		t.Fatalf("Failed to create mock server '%#v'", err)
+	}
+
+	mock.ExpectExec(regexp.QuoteMeta(`INSERT INTO users (email, name, hashed_password) VALUES (?, ?, ?)`)).
+		WithArgs("admin@example.com", "admin", sqlmock.AnyArg()).
+		WillReturnError(fmt.Errorf(""))
+
+	req := httptest.NewRequest(http.MethodPost, "/register", bytes.NewBuffer([]byte("{\"email\": \"admin@example.com\",\"name\": \"admin\",\"password\": \"password\"}")))
+	req.Header.Set("Content-Type", "application/json")
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+
+	users := &users.Users{DB: db}
+	if assert.NoError(t, users.Register(c)) {
+		assert.Equal(t, http.StatusBadRequest, rec.Code)
+		assert.Equal(t, "{\"success\":false,\"error\":\"failed to register to DB\",\"user\":{\"id\":0,\"name\":\"\",\"role\":\"\"}}\n", rec.Body.String())
+	}
+	err = mock.ExpectationsWereMet()
+	if err != nil {
+		t.Errorf("Incorrect DB call '%#v'", err)
+	}
+}

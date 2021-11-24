532a1f2a2e39658e35a7ae07e42ef4eaf2bfe6ba
ecto0310 <ecto0310@gmail.com>
2021-11-25 01:08:44 +0900

    Add session check

--- backend/pkg/test_helper/test_helper.go
+++ backend/pkg/test_helper/test_helper.go
@@ -1,6 +1,12 @@
 package test_helper
 
 import (
+	"bytes"
+	"errors"
+	"net/http"
+	"net/http/httptest"
+	"regexp"
+
 	"github.com/DATA-DOG/go-sqlmock"
 	"github.com/alicebob/miniredis/v2"
 	"github.com/ecto0310/online_judge/backend/pkg/server"
@@ -24,4 +30,23 @@ func CreateTestServer() (*echo.Echo, sqlmock.Sqlmock, error) {
 	server.AddRouting(db, e)
 
 	return e, mock, err
+}
+
+func CreateLoginSession(e *echo.Echo, mock sqlmock.Sqlmock) (string, error) {
+	mock.ExpectQuery(regexp.QuoteMeta(`SELECT hashed_password FROM users WHERE name=?`)).
+		WithArgs("admin").
+		WillReturnRows(sqlmock.NewRows([]string{"hashed_password"}).AddRow("$2a$10$mfgTfkiVqozg7EItYLqp8.jGQ3KVNd9lCQNaITT5zbpEbAvXm7/su"))
+	mock.ExpectQuery(regexp.QuoteMeta(`SELECT id, name, role FROM users WHERE name=?`)).
+		WithArgs("admin").
+		WillReturnRows(sqlmock.NewRows([]string{"id", "name", "role"}).AddRow(1, "admin", "member"))
+
+	req := httptest.NewRequest(http.MethodPost, "/login", bytes.NewBuffer([]byte("{\"name\": \"admin\",\"password\": \"password\"}")))
+	req.Header.Set("Content-Type", "application/json")
+	rec := httptest.NewRecorder()
+	e.ServeHTTP(rec, req)
+
+	if rec.Code != http.StatusOK {
+		return "", errors.New("failed to create session")
+	}
+	return rec.Result().Header.Get("Set-Cookie"), nil
 }
--- backend/pkg/users/logout_test.go
+++ backend/pkg/users/logout_test.go
@@ -16,13 +16,39 @@ func TestLogoutSuccess(t *testing.T) {
 		t.Fatalf("Failed to create mock server '%#v'", err)
 	}
 
+	session, err := test_helper.CreateLoginSession(e, mock)
+	if err != nil {
+		t.Fatalf("Failed to create session '%#v'", err)
+	}
+
 	req := httptest.NewRequest(http.MethodGet, "/logout", bytes.NewBuffer([]byte("")))
 	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Cookie", session)
 	rec := httptest.NewRecorder()
 	e.ServeHTTP(rec, req)
 
 	assert.Equal(t, http.StatusOK, rec.Code)
 	assert.Equal(t, "{\"success\":true,\"error\":\"\"}\n", rec.Body.String())
+
+	err = mock.ExpectationsWereMet()
+	if err != nil {
+		t.Errorf("Incorrect DB call '%#v'", err)
+	}
+}
+
+func TestLogoutNotLogin(t *testing.T) {
+	e, mock, err := test_helper.CreateTestServer()
+	if err != nil {
+		t.Fatalf("Failed to create mock server '%#v'", err)
+	}
+
+	req := httptest.NewRequest(http.MethodGet, "/logout", bytes.NewBuffer([]byte("")))
+	req.Header.Set("Content-Type", "application/json")
+	rec := httptest.NewRecorder()
+	e.ServeHTTP(rec, req)
+
+	assert.Equal(t, http.StatusBadRequest, rec.Code)
+	assert.Equal(t, "{\"success\":false,\"error\":\"Session is not login\"}\n", rec.Body.String())
 
 	err = mock.ExpectationsWereMet()
 	if err != nil {
